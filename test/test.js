869f02d9f094454f484dfb83ea1982840db1e907
jonahokot <jonahrokot2@gmail.com>
2022-09-06 11:16:01 +0300

    first_commit

---
+++ test/test.js
@@ -0,0 +1,19 @@
+const { assert } = require('chai');
+const { it } = require('mocha');
+const factorialize = require('../factorial').default;
+
+// eslint-disable-next-line no-undef
+describe('factorial test', () => {
+  it('Factorial 0', () => {
+    assert.equal(factorialize(1), 1);
+  });
+  it('Factorial 1', () => {
+    assert.equal(factorialize(0), 1);
+  });
+  it('Factorial 4', () => {
+    assert.equal(factorialize(4), 24);
+  });
+  it('Factorial 5', () => {
+    assert.equal(factorialize(5), 120);
+  });
+});
